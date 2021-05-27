609d2ba36bfeb3fa2db17ce43eb42a3a621e2ace
Hitesh Vaidya <hiteshv.tech@gmail.com>
2021-05-26 23:00:55 -0400

    updated SOM_epochs.py
    debugged the program. epoch number was passed to decay functions instead of index numbers.

--- SOM/Hitesh/SOM_epochs.py
+++ SOM/Hitesh/SOM_epochs.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 import os
 import pickle as pkl
-from metadata import config
+# from metadata import config
 import pandas as pd
 import math
 
@@ -26,17 +26,26 @@ def load_data():
     load mnist dataset and relabel according to task number
     :return:    train-test-valid split of data
     """
-    path = '../../mnist_clean/'
-    trainX = read_dataframe(os.path.join(path, 'trainX.tsv'))
-    trainY = read_dataframe(os.path.join(path, 'trainY.tsv'))
+    # path = '../../mnist_clean/'
+    # trainX = read_dataframe(os.path.join(path, 'trainX.tsv'))
+    # trainY = read_dataframe(os.path.join(path, 'trainY.tsv'))
     # testX = read_dataframe(os.path.join(path, 'testX.tsv'))
     # testY = read_dataframe(os.path.join(path, 'testY.tsv'))
     # validX = read_dataframe(os.path.join(path, 'validX.tsv'))
     # validY = read_dataframe(os.path.join(path, 'validY.tsv'))
 
-    trainY = tf.argmax(trainY, axis=1)
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    trainX = tf.cast(tf.reshape(x_train, (x_train.shape[0], -1)), tf.float64)
+    trainY = tf.cast(y_train, tf.float64)
 
-    return trainX, trainY  # , testX, testY, validX, validY
+    trainX = (trainX - tf.math.reduce_mean(trainX)) / tf.math.reduce_std(trainX)
+    tf.print('trainX (max, min) pixels:', (tf.reduce_max(trainX),
+                                           tf.reduce_min(trainX)))
+    tf.print('(mean, std):', (tf.math.reduce_mean(trainX), tf.math.reduce_std(
+        trainX)))
+    # trainY = tf.argmax(trainY, axis=1)
+
+    return trainX, trainY # , testX, testY, validX, validY
 
 
 def find_bmu(img, som):
@@ -56,20 +65,32 @@ def find_bmu(img, som):
     return bmu_idx
 
 
-def decay_radius(initial_radius, i, time_constant):
-    radius = initial_radius * tf.exp(-i / time_constant)
+def decay_radius(initial_radius, current_iter, tau1):
+    current_iter = tf.cast(current_iter, tf.float64)
+    # tf.print('current iter, tau1:', current_iter, current_iter.dtype, ', ',
+    #          tau1, tau1.dtype)
+    # tf.print('power of radius exp term =', (-current_iter / tau1))
+    # print('init_radius =', initial_radius)
+    radius = initial_radius * tf.math.exp(-current_iter / tau1)
+    # tf.print('decayed radius =', radius)
     radius = tf.cast(radius, tf.float64)
     return radius
 
 
-def decay_learning_rate(initial_learning_rate, i, n_iterations):
-    lr = initial_learning_rate * tf.exp(-i / n_iterations)
+def decay_learning_rate(initial_learning_rate, current_iter, tau2):
+    current_iter = tf.cast(current_iter, tf.float64)
+    # tf.print('current iter, tau1:', current_iter, current_iter.dtype,
+    #          ', ', tau2, tau2.dtype)
+    # tf.print('power of lr exp term =', (-current_iter / tau2))
+    # print('init_lr =', initial_learning_rate)
+    lr = initial_learning_rate * tf.math.exp(-current_iter / tau2)
+    # tf.print('decayed lr =', lr, lr.dtype)
     lr = tf.cast(lr, tf.float64)
     return lr
 
 
 def calculate_influence(distance, radius):
-    influence = tf.exp(-distance / (2 * (radius ** 2)))
+    influence = tf.exp(-distance**2 / (2 * (radius ** 2)))
     influence = tf.cast(influence, tf.float64)
     return influence
 
@@ -93,7 +114,9 @@ def plot_som(init_som, final_som):
     fig.colorbar(pos1, ax=ax[1])
     # plt.show()
 
-    plt.savefig('MNIST SOM confusion matrices.png')
+    plt.savefig('som_data/MNIST SOM confusion matrices.png')
+    pkl.dump((init_som, final_som), open('soms_epochs_8_rad_4.pkl', 'wb'))
+
     # plt.imshow(som, interpolation='nearest')
     # if post:
     #     plt.title('Self organizing map after training')
@@ -106,26 +129,34 @@ def plot_som(init_som, final_som):
 
 
 def main():
-    som_dimension = (15, 28 * 28)
-    n_epochs = 20
-    init_learning_rate = 0.1
+    trainX, trainY = load_data()  # , testX, testY, validX, validY
+
+    som_dimension = (20, 28 * 28)
+    n_epochs = 8
+    init_learning_rate = tf.convert_to_tensor(0.1, tf.float64)
     n_classes = 10
+    init_radius = tf.convert_to_tensor(1.19, tf.float64)
+    tau1 = tf.cast(trainX.shape[0], tf.float64) / tf.math.log(
+        init_radius)
+    tau2 = tf.cast(trainX.shape[0], tf.float64)
+    tf.print('tau1, tau2:', tau1, ', ', tau2)
+    print(tau1.dtype, ', ', tau2.dtype)
 
     # initial neighbourhood radius
     # init_radius = max(som_dimension[0], som_dimension[1]) / 2
-    init_radius = 15
     # radius decay parameter
     time_constant = n_epochs / math.log(init_radius)
 
-    trainX, trainY = load_data()  # , testX, testY, validX, validY
 
     # som network
-    som = tf.random.normal(som_dimension, mean=0.0, stddev=0.5, seed=0.0,
+    som = tf.random.normal(som_dimension, mean=0.0, stddev=0.1, seed=0.0,
                            dtype=tf.float64)
+    tf.print('SOM (max, min) values:', (tf.reduce_max(som), tf.reduce_min(som)))
 
     init_som = tf.identity(som)
 
     indices = tf.convert_to_tensor(np.arange(trainX.shape[0]))
+
     for epoch in range(n_epochs):
         # number of times each unit is selected as BMU for each class
         som_count = [{y: 0 for y in range(n_classes)} for x in range(som.shape[
@@ -133,7 +164,8 @@ def main():
         # print('som_count:', som_count)
         avg_bmu_dist_per_epoch = 0
         tf.random.shuffle(indices)
-        for index in indices:
+        tqdm.write('Training indices for epoch ', epoch)
+        for iteration, index in tqdm(enumerate(indices)):
             data = trainX[index]
             label = trainY[index]
             bmu_idx = find_bmu(data, som)
@@ -142,41 +174,51 @@ def main():
             som_count[bmu_idx][label.numpy()] += 1
 
             # decay the SOM parameters
-            radius = decay_radius(init_radius, epoch, time_constant)
-            lr = decay_learning_rate(init_learning_rate, epoch,
-                                     n_epochs)
-
+            radius = decay_radius(init_radius, iteration, tau1)
+            lr = decay_learning_rate(init_learning_rate, iteration,
+                                     tau2)
+            # tf.print('learning rate =', lr)
+            # tf.print('radius =', radius)
             # avg distance of BMU from all other units
             avg_bmu_dist_per_sample = 0
             # temporary som to bypass eager execution error
             temp_som = som.numpy()
             # if changed flag is set, then update SOM
-            changed = False
+            # changed = False
+
             for row, node in enumerate(som):
                 node_dist = tf.norm(som[bmu_idx] - node, ord='euclidean')
+                # tf.print('node distance =', node_dist)
                 avg_bmu_dist_per_sample += node_dist
-                if node_dist <= radius:
-                    # calculate the degree of influence (based on the 2-D distance)
-                    influence = calculate_influence(node_dist, radius)
-                    new_node = node + (lr * influence * (data - node))
-                    temp_som[row] = new_node
-                    changed = True
-            if changed:
-                som = tf.convert_to_tensor(temp_som)
+                # if node_dist < radius:
+                # calculate the degree of influence (based on the 2-D distance)
+                influence = calculate_influence(node_dist, radius)
+                new_node = node + (lr * influence * (data - node))
+                temp_som[row] = new_node
+            #     changed = True
+            # if changed:
+            som = tf.convert_to_tensor(temp_som)
 
             avg_bmu_dist_per_sample /= som.shape[0] - 1
+            # tf.print('Average BMU distance per sample =',
+            #         avg_bmu_dist_per_sample)
             avg_bmu_dist_per_epoch += avg_bmu_dist_per_sample
 
+        avg_bmu_dist_per_epoch /= len(indices)
         print('Epoch ', epoch)
         print('Average BMU distance of all units per sample per epoch:',
-              avg_bmu_dist_per_epoch / len(indices))
+              avg_bmu_dist_per_epoch.numpy())
         print('\nSOM:')
         for index in range(som.shape[0]):
             print('[%s | BMU FOR' % index, end=' ')
             for key, value in som_count[index].items():
-                print('(%d - %d)' % key, value, end=' ')
+                print('(%d - %d)' % (key, value), end=' ')
             print(']')
 
+        pkl.dump((init_som, som), open('som_data/soms.pkl', 'wb'))
+
+    print()
+    plot_som(init_som, som)
 
 if __name__ == '__main__':
     main()
